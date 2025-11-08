d9ee19aae6f08aab2dfdf3f82331e72960ef7c15
Alex Wojtowicz <alex.wojtowicz@proton.me>
2025-11-08 23:46:31 +0100

    chore: refactor sprite as a component

---
+++ DinnEngine/src/DinnCore/Sprite.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "DinnCore/Rendering/Components/Shader.h"
+#include "Component.h"
+#include "Rendering/Texture.h"
+
+namespace Dinn
+{
+	struct Sprite : Component
+	{
+		friend class GameObject;
+
+	public:
+		void SetTexture(std::shared_ptr<Texture> texture) { this->texture = std::move(texture); }
+		void SetShader(std::shared_ptr<Shader> shader) { this->shader = std::move(shader); }
+
+		void ClearTexture() { texture.reset(); }
+		void ClearShader() { shader.reset(); }
+
+		std::shared_ptr<Texture> GetTexture() const { return texture.lock(); }
+		std::shared_ptr<Shader> GetShader() const { return shader.lock();}
+
+		bool HasTextureOverride() { return !texture.expired(); }
+		bool HasShaderOverrride() {	return !shader.expired(); }
+
+	protected:
+		explicit Sprite(GameObject& owner) 
+			: Component(owner) {}
+		explicit Sprite(GameObject& owner, std::shared_ptr<Texture> texture) 
+			: Component(owner), texture(texture) {}
+		explicit Sprite(GameObject& owner, std::shared_ptr<Shader> shader) 
+			: Component(owner), shader(shader) {}
+		explicit Sprite(GameObject& owner, std::shared_ptr<Texture> texture, std::shared_ptr<Shader> shader)
+			: Component(owner), texture(texture), shader(shader) {}
+
+		std::weak_ptr<Shader> shader;
+		std::weak_ptr<Texture> texture;
+	};
+}
+
